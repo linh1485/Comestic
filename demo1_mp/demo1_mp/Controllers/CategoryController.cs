638bc7b98c7d8f4c7395e59685c8418a33d80895
linh1485 <doailinh2003@gmail.com>
2023-09-28 12:59:59 +0700

    kết nối db vào máy

---
+++ demo1_mp/demo1_mp/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo1_mp.Models;
+
+
+namespace demo1_mp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private dbEntities db = new dbEntities();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ demo1_mp/demo1_mp/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo1_mp.Models;
+
+namespace demo1_mp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private dbEntities db = new dbEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
